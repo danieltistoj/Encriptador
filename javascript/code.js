460891f7b10633989e1f249b535eb8edb2f6d741
danieltistoj <josetisrey@gmail.com>
2023-05-16 23:25:26 -0600

    agregar evento al boton desencriptar

--- javascript/code.js
+++ javascript/code.js
@@ -1,5 +1,15 @@
-//Este seria el evento click 
+//Evento del boton Encriptar
 document.getElementById("buttonEncrypt").addEventListener("click", function(){
+    //de esta forma capturamos lo que este en el texarea
+    var textareaContent = document.getElementById("myTextArea").value;
+    if(textareaContent == ""){
+        alert("Debe de escribir algo")
+    }else{
+        alert("Contenido del text area: "+textareaContent)
+    }
+})
+//Evento del boton desencriptar
+document.getElementById("buttonDecrypt").addEventListener("click", function(){
     //de esta forma capturamos lo que este en el texarea
     var textareaContent = document.getElementById("myTextArea").value;
     if(textareaContent == ""){
