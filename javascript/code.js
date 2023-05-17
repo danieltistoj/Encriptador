8e9ece8990f83aa94b7be318600330dff49496ba
danieltistoj <josetisrey@gmail.com>
2023-05-17 15:44:04 -0600

    funcion encriptar y retorno de palabra

--- javascript/code.js
+++ javascript/code.js
@@ -1,3 +1,30 @@
+var arrayVowels = ["a","e","i","o","u"]
+var arrayEncrypt = ["ai","enter","imes","ober","ufat"]
+
+function encrypt(text){
+    var separateText = []
+    //recorremos el texto
+    for(var i=0; i<text.length; i++){
+        console.log(text[i])
+        var exists = false
+        //verificamos que la letra este entre las letras que se deben de encriptar 
+        for(var j=0; j<arrayVowels.length; j++){
+            if(text[i]==arrayVowels[j]){
+                //si la encuentra agregamos el texto encriptado segun la letra 
+                console.log(text[i],arrayEncrypt[j]);
+                separateText.push(arrayEncrypt[j])
+                exists = true
+            }
+        }
+        if(!exists){
+            separateText.push(text[i]);
+        }   
+    }
+    return separateText.join("")
+}
+function decrypt(text){
+
+}
 //Evento del boton Encriptar
 document.getElementById("buttonEncrypt").addEventListener("click", function(){
     //de esta forma capturamos lo que este en el texarea
@@ -5,7 +32,7 @@ document.getElementById("buttonEncrypt").addEventListener("click", function(){
     if(textareaContent == ""){
         alert("Debe de escribir algo")
     }else{
-        alert("Contenido del text area: "+textareaContent)
+        console.log(encrypt(textareaContent))
     }
 })
 //Evento del boton desencriptar
